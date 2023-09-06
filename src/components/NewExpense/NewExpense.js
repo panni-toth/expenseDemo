762bc12c48f5daea1072347f8c7ef8edf1288fa7
ufuk <ufuk.algac@campus.tu-berlin.de>
2023-09-06 20:11:14 +0200

    initial commit to upload the template

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import "./NewExpense.css";
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css';
+
+
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm/>
+    </div>
+};
+
+export default NewExpense;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import './Card.css';
+
+function Card(props){
+    
+    const classes = props.className;
+   
+    return (<div className={classes}> {props.children}</div>)
+ 
+ }
+ export default Card;
